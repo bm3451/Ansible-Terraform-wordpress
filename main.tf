f4f1eae1711e551d6f6bec6f278b2722ba4db742
seoyoung <bm3451@naver.com>
2021-08-09 06:06:22 +0000

    debug: wordpress-database error

--- main.tf
+++ main.tf
@@ -43,9 +43,9 @@ resource "aws_db_instance" "my_db" {
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
-  name                 = "mydb"
+  name                 = "wpdb"
   username             = "wpadm"
-  password             = "hwangsy98!"
+  password             = "hwangsy98"
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
   publicly_accessible  = true
